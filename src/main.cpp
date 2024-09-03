bcf435cae2e32dfbc31ff9bed730e24904aea722
vsemi <horace.li@vsemi.io>
2024-09-03 13:55:51 -0400

    2.1.3

--- src/main.cpp
+++ src/main.cpp
@@ -512,7 +512,7 @@ int send_message()
 			time_t t = std::time(nullptr);
 
 			std::string payload = "{\"device_id\":\"" + std::to_string(sensor_uid) + "\", \"msg_type\":\"ping\", \"dt\":\"" + std::to_string(t) + "\"";
-			payload += ", \"version\": \"2.1.2\"";
+			payload += ", \"version\": \"2.1.3\"";
 			payload += "}";
 
 			send_message_payload(payload);
